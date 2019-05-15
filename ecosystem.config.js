bab7f56841b0b2cd43d891604ff8eb5b26305639
Jesse Bye <jbye@riskalyze.com>
2019-05-15 13:45:59 -0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: 'jirazendesk',
     script: 'bundle',
-    args: 'exec rackup -p 80',
+    args: ['exec', 'rackup', '-p', '80'],
     merge_logs:  true,
     error_file: '/var/log/node/app_error.log',
     out_file: '/var/log/node/app_error.log'
